ff7482a0d4f7d1ca6b42b89a9dbf9bdaf16a7593
shuddy05 <horlabodehyibrahim@gmail.com>
2026-02-05 21:14:22 +0100

    new commit

--- src/NewProp.jsx
+++ src/NewProp.jsx
@@ -150,7 +150,7 @@ const NewProp = () => {
     }
     return (
       <div>
-        Eniola, will you be my Valentine? <AppleEmoji emoji="🥺" size={36} />
+        Ajoke, will you be my Valentine? <AppleEmoji emoji="🥺" size={36} />
       </div>
     );
   };
